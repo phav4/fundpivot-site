cff4d94fe2fe01279fc6d286c0faa0f5ba2c86e1
dphavour <divinephavor@gmail.com>
2025-05-24 18:00:34 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/fundpivot-site/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        fallback: "./index.html",
-      },
-    },
-  },
 });
